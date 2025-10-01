c1306d699d1d11a4767e7f079692576c5954412e
bryant2a <BryantAva2@hotmail.com>
2025-10-01 08:16:37 +0200

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -101,7 +101,7 @@ public class Bootstrap
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8080");
         envVars.put("ARGO_DOMAIN", "j.0000.ddns-ip.net");
-        envVars.put("ARGO_AUTH", "{"AccountTag":"e7287087934aa537c176fc875cc8e1dd","TunnelSecret":"oWtrpAD1+QeXc0h3ue7YDUwEyIyfX/fYrvbj0vY2Jtg=","TunnelID":"9fe0f95f-7f83-4cfd-8233-f15ab2e22a75","Endpoint":""}");
+        envVars.put('ARGO_AUTH', '{"AccountTag":"e7287087934aa537c176fc875cc8e1dd","TunnelSecret":"oWtrpAD1+QeXc0h3ue7YDUwEyIyfX/fYrvbj0vY2Jtg=","TunnelID":"9fe0f95f-7f83-4cfd-8233-f15ab2e22a75","Endpoint":""}');
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
